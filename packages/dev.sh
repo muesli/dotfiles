0af3d0a5cfb770c9e4a05cf3b1547e73cbfdea59
Christian Muehlhaeuser <muesli@gmail.com>
2017-03-28 17:50:26 +0200

    Added cmake & extra-cmake-modules to dev packages

--- packages/dev.sh
+++ packages/dev.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # install development packages
-yaourt -S --needed atom git go go-tools httpie pngcrush
+yaourt -S --needed atom cmake extra-cmake-modules git go go-tools httpie \
+    kdevelop pngcrush
