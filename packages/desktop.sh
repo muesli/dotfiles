6957fb5cd14347f09ff2d36d90b6d034424e4c41
Christian Muehlhaeuser <muesli@gmail.com>
2017-03-28 15:24:54 +0200

    Make sure qt4 gets installed for vlc's GUI version

--- packages/desktop.sh
+++ packages/desktop.sh
@@ -3,5 +3,5 @@
 # install desktop packages
 yaourt -S --needed \
     chromium-widevine dolphin gwenview kate kcalc kdevelop konsole \
-    konversation kwrite phonon-qt5-vlc plasma-meta telegram-desktop-bin \
+    konversation kwrite phonon-qt5-vlc plasma-meta qt4 telegram-desktop-bin \
     trash-cli ttf-liberation vlc yakuake
