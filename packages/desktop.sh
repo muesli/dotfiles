1784b15f22979a718cd2c7999ce9aeeb26824b15
Christian Muehlhaeuser <muesli@gmail.com>
2017-03-28 17:50:02 +0200

    Added kdialog & plasma5-themes-chakra-heritage to desktop packages

--- packages/desktop.sh
+++ packages/desktop.sh
@@ -2,6 +2,6 @@
 
 # install desktop packages
 yaourt -S --needed \
-    chromium-widevine dolphin gwenview kate kcalc kdevelop konsole \
-    konversation kwrite phonon-qt5-vlc plasma-meta qt4 telegram-desktop-bin \
-    trash-cli ttf-liberation vlc yakuake
+    chromium-widevine dolphin gwenview kate kcalc kdialog konsole konversation \
+    kwrite phonon-qt5-vlc plasma-meta plasma5-themes-chakra-heritage qt4 \
+    telegram-desktop-bin trash-cli ttf-liberation vlc yakuake
