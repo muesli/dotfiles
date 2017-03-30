c29651c0389581f1ab196e5ddd03491c6ed1eecf
Christian Muehlhaeuser <muesli@gmail.com>
2017-03-30 02:55:05 +0200

    Replaced systemd-kcm with systemdgenie

--- packages/desktop.sh
+++ packages/desktop.sh
@@ -4,4 +4,4 @@
 yaourt -S --needed \
     chromium-widevine dolphin gwenview kate kcalc kdialog konsole konversation \
     kwrite phonon-qt5-vlc plasma-meta plasma5-themes-chakra-heritage qt4 \
-    systemd-kcm telegram-desktop-bin trash-cli ttf-liberation vlc yakuake
+    systemdgenie telegram-desktop-bin trash-cli ttf-liberation vlc yakuake
