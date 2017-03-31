625aa9c9c06dd658624ce587e7dc86eb4bf5b34f
Christian Muehlhaeuser <muesli@gmail.com>
2017-03-31 05:00:29 +0200

    Added ark & obs-studio to desktop packages

--- packages/desktop.sh
+++ packages/desktop.sh
@@ -2,7 +2,7 @@
 
 # install desktop packages
 yaourt -S --needed \
-    chromium-widevine dolphin gwenview kate kcalc kdeconnect kdialog konsole \
-    konversation kwrite phonon-qt5-vlc plasma-meta \
+    ark chromium-widevine dolphin gwenview kate kcalc kdeconnect kdialog \
+    konsole konversation kwrite obs-studio phonon-qt5-vlc plasma-meta \
     plasma5-themes-chakra-heritage qt4 systemdgenie telegram-desktop-bin \
     trash-cli ttf-liberation vlc yakuake
