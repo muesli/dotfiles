417600d25061f0c41ad9f3cf1e94849279ca96d8
Christian Muehlhaeuser <muesli@gmail.com>
2017-03-31 05:07:00 +0200

    Added krita, ksysguard, ksystemlog to desktop packages

--- packages/desktop.sh
+++ packages/desktop.sh
@@ -3,6 +3,6 @@
 # install desktop packages
 yaourt -S --needed \
     ark chromium-widevine dolphin gwenview kate kcalc kdeconnect kdialog \
-    konsole konversation kwrite obs-studio phonon-qt5-vlc plasma-meta \
-    plasma5-themes-chakra-heritage qt4 systemdgenie telegram-desktop-bin \
-    trash-cli ttf-liberation vlc yakuake
+    konsole konversation krita ksysguard ksystemlog kwrite obs-studio \
+    phonon-qt5-vlc plasma-meta plasma5-themes-chakra-heritage qt4 systemdgenie \
+    telegram-desktop-bin trash-cli ttf-liberation vlc yakuake
