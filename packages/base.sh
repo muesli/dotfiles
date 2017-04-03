5dd3eaa3b2a6f534d034a1269d11c7625f251ce0
Christian Muehlhaeuser <muesli@gmail.com>
2017-04-03 17:17:52 +0200

    Added nmap to base packages

--- packages/base.sh
+++ packages/base.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 PACKAGES=("${PACKAGES[@]}" "coreutils" "dosfstools" "ethtool" "hdparm" "htop" \
-    "iotop" "joe" "ncdu" "net-tools" "openssh" "tmux" "traceroute" "unrar" \
-    "unzip" "which" "wol" "yay" "zsh")
+    "iotop" "joe" "ncdu" "net-tools" "nmap" "openssh" "tmux" "traceroute" \
+    "unrar" "unzip" "which" "wol" "yay" "zsh")
