54eae8ae6d461fa5ea285a1b7fd9f8c1b9adf60a
Christian Muehlhaeuser <muesli@gmail.com>
2017-04-05 10:56:35 +0200

    Added kaddressbook to desktop packages

--- packages/desktop.sh
+++ packages/desktop.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 PACKAGES=("${PACKAGES[@]}" "ark" "chromium-widevine" "dolphin" "frei0r-plugins" \
-    "gwenview" "kate" "kcalc" "kdeconnect" "kdenlive" "kdialog" "kmail" \
-    "konsole" "kontact" "konversation" "krita" "ksysguard" "ksystemlog" \
-    "kwalletmanager" "kwrite" "obs-studio" "okular" "phonon-qt5-vlc" \
-    "plasma-meta" "plasma5-themes-chakra-heritage" "qt4" "systemdgenie" \
-    "telegram-desktop-bin" "trash-cli" "ttf-liberation" "vlc" "yakuake")
+    "gwenview" "kaddressbook" "kate" "kcalc" "kdeconnect" "kdenlive" "kdialog" \
+    "kmail" "konsole" "kontact" "konversation" "korganizer" "krita" "ksysguard" \
+    "ksystemlog" "kwalletmanager" "kwrite" "obs-studio" "okular" \
+    "phonon-qt5-vlc" "plasma-meta" "plasma5-themes-chakra-heritage" "qt4" \
+    "systemdgenie" "telegram-desktop-bin" "trash-cli" "ttf-liberation" "vlc" \
+    "yakuake")
