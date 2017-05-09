31d2a74f835c6f7480ea6d677c8f70c16d6e87b1
Christian Muehlhaeuser <muesli@gmail.com>
2017-05-09 20:01:32 +0200

    Added pavucontrol-qt to desktop packages

--- packages/desktop.sh
+++ packages/desktop.sh
@@ -4,6 +4,6 @@ PACKAGES=("${PACKAGES[@]}" "ark" "chromium-widevine" "dolphin" "frei0r-plugins" 
     "gwenview" "kaddressbook" "kate" "kcalc" "kdeconnect" "kdenlive" "kdialog" \
     "kmail" "konsole" "kontact" "konversation" "korganizer" "krita" "ksysguard" \
     "ksystemlog" "kwalletmanager" "kwrite" "obs-studio" "okular" \
-    "phonon-qt5-vlc" "plasma-meta" "plasma5-themes-chakra-heritage" "qt4" \
-    "systemdgenie" "telegram-desktop-bin" "trash-cli" "ttf-liberation" "vlc" \
-    "yakuake")
+    "pavucontrol-qt" "phonon-qt5-vlc" "plasma-meta" \
+    "plasma5-themes-chakra-heritage" "qt4" "systemdgenie" "telegram-desktop-bin" \
+    "trash-cli" "ttf-liberation" "vlc" "yakuake")
