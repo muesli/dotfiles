f79549c417fcea4d7ecaa9dedb3fef6657ae040f
Christian Muehlhaeuser <muesli@gmail.com>
2017-05-16 02:48:37 +0200

    Added avahi to desktop packages

--- packages/desktop.sh
+++ packages/desktop.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-PACKAGES=("${PACKAGES[@]}" "ark" "chromium-widevine" "dolphin" "frei0r-plugins" \
-    "gwenview" "kaddressbook" "kate" "kcalc" "kdeconnect" "kdenlive" "kdialog" \
-    "kmail" "konsole" "kontact" "konversation" "korganizer" "krita" "ksysguard" \
-    "ksystemlog" "kwalletmanager" "kwrite" "obs-studio" "okular" \
-    "pavucontrol-qt" "phonon-qt5-vlc" "plasma-meta" \
+PACKAGES=("${PACKAGES[@]}" "ark" "avahi" "chromium-widevine" "dolphin" \
+    "frei0r-plugins" "gwenview" "kaddressbook" "kate" "kcalc" "kdeconnect" \
+    "kdenlive" "kdialog" "kmail" "konsole" "kontact" "konversation" \
+    "korganizer" "krita" "ksysguard" "ksystemlog" "kwalletmanager" "kwrite" \
+    "obs-studio" "okular" "pavucontrol-qt" "phonon-qt5-vlc" "plasma-meta" \
     "plasma5-themes-chakra-heritage" "pulseaudio-alsa" "pulseaudio-zeroconf" \
     "qt4" "systemdgenie" "telegram-desktop-bin" "trash-cli" "ttf-liberation" \
     "vlc" "yakuake")
