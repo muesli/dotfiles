49465cf7ce1326cb06948c7bb61d89efb06c27ec
Christian Muehlhaeuser <muesli@gmail.com>
2017-06-07 21:01:49 +0200

    Added dnsutils to base packages

--- packages/base.sh
+++ packages/base.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-PACKAGES=("${PACKAGES[@]}" "coreutils" "dosfstools" "ethtool" "hdparm" "htop" \
-    "iotop" "joe" "keybase" "lsof" "ncdu" "net-tools" "nmap" "openssh" "tmux" \
-    "traceroute" "unrar" "unzip" "which" "wol" "yay" "zsh")
+PACKAGES=("${PACKAGES[@]}" "coreutils" "dnsutils" "dosfstools" "ethtool" \
+    "hdparm" "htop" "iotop" "joe" "keybase" "lsof" "ncdu" "net-tools" "nmap" \
+    "openssh" "tmux" "traceroute" "unrar" "unzip" "which" "wol" "yay" "zsh")
