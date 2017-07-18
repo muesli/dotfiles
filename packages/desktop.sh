86b0a3cf414f5f2308ba44c6923ddeeccc351c02
Christian Muehlhaeuser <muesli@gmail.com>
2017-07-18 23:48:41 +0200

    Added browserpass and gopass to desktop packages

--- packages/desktop.sh
+++ packages/desktop.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-PACKAGES=("${PACKAGES[@]}" "ark" "avahi" "chromium-widevine" "dolphin" \
-    "frei0r-plugins" "gwenview" "kaddressbook" "kate" "kcalc" "kdeconnect" \
-    "kdenlive" "kdialog" "kmail" "konsole" "kontact" "konversation" \
-    "korganizer" "krita" "ksysguard" "ksystemlog" "kwalletmanager" "kwrite" \
-    "obs-studio" "okular" "pavucontrol-qt" "phonon-qt5-vlc" "plasma-meta" \
-    "plasma5-themes-chakra-heritage" "pulseaudio-alsa" "pulseaudio-zeroconf" \
-    "qt4" "sshfs" "systemdgenie" "telegram-desktop-bin" "trash-cli" \
-    "ttf-liberation" "vlc" "yakuake")
+PACKAGES=("${PACKAGES[@]}" "ark" "avahi" "browserpass" "chromium-widevine" \
+    "dolphin" "frei0r-plugins" "gopass" "gwenview" "kaddressbook" "kate" \
+    "kcalc" "kdeconnect" "kdenlive" "kdialog" "kmail" "konsole" "kontact" \
+    "konversation" "korganizer" "krita" "ksysguard" "ksystemlog" \
+    "kwalletmanager" "kwrite" "obs-studio" "okular" "pavucontrol-qt" \
+    "phonon-qt5-vlc" "plasma-meta" "plasma5-themes-chakra-heritage" \
+    "pulseaudio-alsa" "pulseaudio-zeroconf" "qt4" "sshfs" "systemdgenie" \
+    "telegram-desktop-bin" "trash-cli" "ttf-liberation" "vlc" "yakuake")
