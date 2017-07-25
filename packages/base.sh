068b1b1ade352f1ec5fa08b115593ac1055b5394
Christian Muehlhaeuser <muesli@gmail.com>
2017-07-25 03:57:12 +0200

    Added nss-mdns to base packages

--- packages/base.sh
+++ packages/base.sh
@@ -2,5 +2,5 @@
 
 PACKAGES=("${PACKAGES[@]}" "coreutils" "dnsutils" "dosfstools" "ethtool" \
     "hdparm" "htop" "iotop" "joe" "keybase" "lsof" "ncdu" "net-tools" "nmap" \
-    "openbsd-netcat" "openssh" "tmux" "traceroute" "unrar" "unzip" "which" \
-    "wol" "yay" "zsh")
+    "nss-mdns" "openbsd-netcat" "openssh" "tmux" "traceroute" "unrar" "unzip" \
+    "which" "wol" "yay" "zsh")
