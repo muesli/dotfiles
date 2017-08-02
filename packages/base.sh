07a2723db6c15b3aafb4e6ddc8492d451a910892
Christian Muehlhaeuser <muesli@gmail.com>
2017-08-02 06:46:18 +0200

    Added time to base packages

--- packages/base.sh
+++ packages/base.sh
@@ -2,5 +2,5 @@
 
 PACKAGES=("${PACKAGES[@]}" "coreutils" "dnsutils" "dosfstools" "ethtool" \
     "hdparm" "htop" "iotop" "joe" "keybase" "lsof" "ncdu" "net-tools" "nmap" \
-    "nss-mdns" "openbsd-netcat" "openssh" "tmux" "traceroute" "unrar" "unzip" \
-    "which" "wol" "yay" "zsh")
+    "nss-mdns" "openbsd-netcat" "openssh" "time" "tmux" "traceroute" "unrar" \
+    "unzip" "which" "wol" "yay" "zsh")
