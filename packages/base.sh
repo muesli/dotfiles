e47a4f5d1d37051ec4fd93877bdac599039a6dff
Christian Muehlhaeuser <muesli@gmail.com>
2017-08-07 20:22:30 +0200

    Added micro to base packages

--- packages/base.sh
+++ packages/base.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 PACKAGES=("${PACKAGES[@]}" "coreutils" "dnsutils" "dosfstools" "ethtool" \
-    "hdparm" "htop" "iotop" "joe" "keybase" "lsof" "ncdu" "net-tools" "nmap" \
-    "nss-mdns" "openbsd-netcat" "openssh" "smartmontools" "time" "tmux" \
+    "hdparm" "htop" "iotop" "joe" "keybase" "lsof" "micro" "ncdu" "net-tools" \
+    "nmap" "nss-mdns" "openbsd-netcat" "openssh" "smartmontools" "time" "tmux" \
     "traceroute" "unrar" "unzip" "which" "wol" "yay" "zsh")
