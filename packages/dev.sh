bb63d3083f10d93d988c941b7d9e32f7c0481a12
Christian Muehlhaeuser <muesli@gmail.com>
2017-08-27 18:48:36 +0200

    Added visual-studio-code-oss to dev packages

--- packages/dev.sh
+++ packages/dev.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 PACKAGES=("${PACKAGES[@]}" "atom" "cmake" "extra-cmake-modules" "git" "go" \
-    "go-tools" "httpie" "js-beautify" "kdevelop" "pngcrush")
+    "go-tools" "httpie" "js-beautify" "kdevelop" "pngcrush" \
+    "visual-studio-code-oss")
