b4dc67f5599953b3808d15893d67749b6abfdded
Christian Muehlhaeuser <muesli@gmail.com>
2017-08-27 19:07:55 +0200

    Added qt5 to dev packages

--- packages/dev.sh
+++ packages/dev.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 PACKAGES=("${PACKAGES[@]}" "atom" "cmake" "extra-cmake-modules" "git" "go" \
-    "go-tools" "httpie" "js-beautify" "kdevelop" "pngcrush" \
+    "go-tools" "httpie" "js-beautify" "kdevelop" "pngcrush" "qt5-base" \
+    "qt5-systems" "qt5-feedback" "qt5-xmlpatterns" "qt5-x11extras" \
+    "qt5-webview" "qt5-webchannel" "qt5-websockets" "qt5-webkit" "qt5-webengine"
+    "qt5-tools" "qt5-svg" "qt5-speech" "qt5-serialport" "qt5-serialbus" \
+    "qt5-sensors" "qt5-script" "qt5-remoteobjects" "qt5-quickcontrols2" \
+    "qt5-networkauth" "qt5-multimedia" "qt5-location" "qt5-imageformats" \
+    "qt5-graphicaleffects" "qt5-gamepad" "qt5-doc" "qt5-declarative" \
+    "qt5-datavis3d" "qt5-connectivity" "qt5-charts" "qt5-canvas3d" "qt5-3d" \
     "visual-studio-code-oss")
