3a041b04c40a70308a18f906269c954c579cdf4c
Christian Muehlhaeuser <muesli@gmail.com>
2017-09-06 12:36:54 +0200

    In preparation for a switch from yaourt to yay, added installYay helper

--- packages/packages.sh
+++ packages/packages.sh
@@ -3,9 +3,22 @@
 # Get base-dir of this shell script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+function installYay {
+    pwd="$(pwd)"
+
+    mkdir /tmp/yah
+    cd /tmp/yay
+    curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz
+    tar -xvzf yay.tar.gz
+    cd yay
+    makepkg -si
+
+    cd "$pwd"
+}
+
 function installYaourt {
-    pwd="`pwd`"
-    # install yaourt
+    pwd="$(pwd)"
+
     mkdir /tmp/yaourt
     cd /tmp/yaourt
     curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz
