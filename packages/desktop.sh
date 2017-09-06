21021e5d84fdfc6185e7c343beb435c41e612271
Christian Muehlhaeuser <muesli@gmail.com>
2017-09-06 05:22:36 +0200

    Added plasma-applets-hoppla-sa-git to desktop packages

--- packages/desktop.sh
+++ packages/desktop.sh
@@ -5,7 +5,7 @@ PACKAGES=("${PACKAGES[@]}" "ark" "avahi" "browserpass" "chromium-widevine" \
     "kcalc" "kdeconnect" "kdenlive" "kdialog" "kleopatra" "kmail" "konsole" \
     "kontact" "konversation" "korganizer" "krita" "ksysguard" "ksystemlog" \
     "kwalletmanager" "kwrite" "noto-fonts-emoji" "obs-studio" "okular" \
-    "pavucontrol-qt" "phonon-qt5-vlc" "plasma-meta" \
-    "plasma5-themes-chakra-heritage" "pulseaudio-alsa" "pulseaudio-zeroconf" \
-    "qt4" "sshfs" "systemdgenie" "telegram-desktop-bin" "trash-cli" \
-    "ttf-liberation" "vlc" "yakuake")
+    "pavucontrol-qt" "phonon-qt5-vlc" "plasma5-applets-hoppla-sa-git" \
+    "plasma-meta" "plasma5-themes-chakra-heritage" "pulseaudio-alsa" \
+    "pulseaudio-zeroconf" "qt4" "sshfs" "systemdgenie" "telegram-desktop-bin" \
+    "trash-cli" "ttf-liberation" "vlc" "yakuake")
