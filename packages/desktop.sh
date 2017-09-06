99c70d9b35c7d03342818be37a57a43f238f6248
Christian Muehlhaeuser <muesli@gmail.com>
2017-09-06 13:09:24 +0200

    Added falkon-git to desktop packages

--- packages/desktop.sh
+++ packages/desktop.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 PACKAGES=("${PACKAGES[@]}" "ark" "avahi" "browserpass" "chromium-widevine" \
-    "dolphin" "frei0r-plugins" "gopass" "gwenview" "kaddressbook" "kate" \
-    "kcalc" "kdeconnect" "kdenlive" "kdialog" "kleopatra" "kmail" "konsole" \
-    "kontact" "konversation" "korganizer" "krita" "ksysguard" "ksystemlog" \
-    "kwalletmanager" "kwrite" "noto-fonts-emoji" "obs-studio" "okular" \
-    "pavucontrol-qt" "phonon-qt5-vlc" "plasma5-applets-hoppla-sa-git" \
+    "dolphin" "falkon-git" "frei0r-plugins" "gopass" "gwenview" "kaddressbook" \
+    "kate" "kcalc" "kdeconnect" "kdenlive" "kdialog" "kleopatra" "kmail" \
+    "konsole" "kontact" "konversation" "korganizer" "krita" "ksysguard" \
+    "ksystemlog" "kwalletmanager" "kwrite" "noto-fonts-emoji" "obs-studio" \
+    "okular" "pavucontrol-qt" "phonon-qt5-vlc" "plasma5-applets-hoppla-sa-git" \
     "plasma-meta" "plasma5-themes-chakra-heritage" "pulseaudio-alsa" \
     "pulseaudio-zeroconf" "qt4" "sshfs" "systemdgenie" "telegram-desktop-bin" \
     "trash-cli" "ttf-liberation" "vlc" "yakuake")
