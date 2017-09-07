8335d8c0e83eadf8efa8e303d90a0c04463b5079
Christian Muehlhaeuser <muesli@gmail.com>
2017-09-08 01:24:47 +0200

    Added ripgrep to base packages

--- packages/arch/base.sh
+++ packages/arch/base.sh
@@ -2,5 +2,5 @@
 
 PACKAGES=("${PACKAGES[@]}" "coreutils" "dnsutils" "dosfstools" "ethtool" \
     "hdparm" "htop" "iotop" "joe" "keybase" "lsof" "micro" "ncdu" "net-tools" \
-    "nmap" "nss-mdns" "openbsd-netcat" "openssh" "smartmontools" "time" "tmux" \
-    "traceroute" "unrar" "unzip" "which" "wol" "yay" "zsh")
+    "nmap" "nss-mdns" "openbsd-netcat" "openssh" "ripgrep" "smartmontools" \
+    "time" "tmux" "traceroute" "unrar" "unzip" "which" "wol" "yay" "zsh")
