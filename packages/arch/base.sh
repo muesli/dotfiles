f273010f1ffce39f2588716605791d9655a83afe
Christian Muehlhaeuser <muesli@gmail.com>
2017-12-05 11:22:10 +0100

    Switched from micro to micro-bin on Arch

--- packages/arch/base.sh
+++ packages/arch/base.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PACKAGES=("${PACKAGES[@]}" "coreutils" "dnsutils" "dosfstools" "ethtool" \
-    "hdparm" "htop" "iotop" "joe" "keybase" "lsof" "micro" "ncdu" "net-tools" \
-    "nmap" "nss-mdns" "openbsd-netcat" "openssh" "ripgrep" "smartmontools" \
-    "time" "tmux" "traceroute" "unrar" "unzip" "which" "wol" "yay" "zsh")
+    "hdparm" "htop" "iotop" "joe" "keybase" "lsof" "micro-bin" "ncdu" \
+    "net-tools" "nmap" "nss-mdns" "openbsd-netcat" "openssh" "ripgrep" \
+    "smartmontools" "time" "tmux" "traceroute" "unrar" "unzip" "which" "wol" \
+    "yay" "zsh")
