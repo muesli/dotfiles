fc90531540789b236c2e3462fd72f08ba9566016
Christian Muehlhaeuser <muesli@gmail.com>
2018-01-07 15:27:22 +0100

    visual-studio-code-oss is now just code in AUR

--- packages/arch/dev.sh
+++ packages/arch/dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PACKAGES=("${PACKAGES[@]}" "atom" "cmake" "extra-cmake-modules" "git" "go" \
-    "go-tools" "httpie" "js-beautify" "kdevelop" "pngcrush" "qt5-base" \
+PACKAGES=("${PACKAGES[@]}" "atom" "cmake" "code" "extra-cmake-modules" "git" \
+    "go" "go-tools" "httpie" "js-beautify" "kdevelop" "pngcrush" "qt5-base" \
     "qt5-systems" "qt5-feedback" "qt5-xmlpatterns" "qt5-x11extras" \
     "qt5-webview" "qt5-webchannel" "qt5-websockets" "qt5-webkit" "qt5-webengine"
     "qt5-tools" "qt5-svg" "qt5-speech" "qt5-serialport" "qt5-serialbus" \
@@ -9,4 +9,8 @@ PACKAGES=("${PACKAGES[@]}" "atom" "cmake" "extra-cmake-modules" "git" "go" \
     "qt5-networkauth" "qt5-multimedia" "qt5-location" "qt5-imageformats" \
     "qt5-graphicaleffects" "qt5-gamepad" "qt5-doc" "qt5-declarative" \
     "qt5-datavis3d" "qt5-connectivity" "qt5-charts" "qt5-canvas3d" "qt5-3d" \
-    "qt5-scxml" "tokei" "visual-studio-code-oss")
+    "qt5-scxml" "tokei")
+
+# To be added:
+#
+#   "gostatus"
