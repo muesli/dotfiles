46c25ae0d1edaee06d7ea400d7349bc0df780785
Christian Muehlhaeuser <muesli@gmail.com>
2018-04-18 01:57:22 +0200

    Removed obsolete installYaourt helper

--- packages/arch/packages.sh
+++ packages/arch/packages.sh
@@ -18,27 +18,6 @@ function installYay {
     cd "$pwd"
 }
 
-function installYaourt {
-    pwd="$(pwd)"
-
-    sudo pacman -S --noconfirm --needed base-devel diffutils wget
-
-    mkdir /tmp/yaourt
-    cd /tmp/yaourt
-    curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz
-    tar -xvzf package-query.tar.gz
-    cd package-query
-    makepkg -si
-
-    cd ..
-    curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz
-    tar -xvzf yaourt.tar.gz
-    cd yaourt
-    makepkg -si
-
-    cd "$pwd"
-}
-
 # Update system
 sudo pacman -Syu
 
