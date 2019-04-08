4170f5f89a3f7c383a92d7fb1db2bb2f1678f997
Christian Muehlhaeuser <muesli@gmail.com>
2019-04-08 14:17:12 +0200

    Added more default TTF fonts

--- packages/arch/desktop.sh
+++ packages/arch/desktop.sh
@@ -12,7 +12,7 @@ PACKAGES=("${PACKAGES[@]}"
     # Video players
     "mpv" "vlc"
     # Extra fonts
-    "noto-fonts-emoji" "ttf-liberation" "ttf-roboto" "ttf-ms-fonts"
+    "noto-fonts-emoji" "ttf-freefont" "ttf-liberation" "ttf-roboto" "ttf-ms-fonts"
     # OBS
     "obs-studio" "frei0r-plugins"
     # Audio stuff
