a9a1255826f62b471d168907662ecf659696b386
Christian Muehlhaeuser <muesli@gmail.com>
2019-04-27 00:19:24 +0200

    Added wget to base packages

--- packages/arch/base.sh
+++ packages/arch/base.sh
@@ -6,7 +6,7 @@ PACKAGES=("${PACKAGES[@]}"
     # Package utils
     "pacutils"
     # Network tools
-    "ethtool" "nmap" "nss-mdns" "openbsd-netcat" "openssh" "prettyping" "sshfs" "traceroute" "whois" "wol"
+    "ethtool" "nmap" "nss-mdns" "openbsd-netcat" "openssh" "prettyping" "sshfs" "traceroute" "wget" "whois" "wol"
     # Disk drive utils
     "hdparm" "smartmontools"
     # Monitors
--- packages/arch_minimal/base.sh
+++ packages/arch_minimal/base.sh
@@ -6,7 +6,7 @@ PACKAGES=("${PACKAGES[@]}"
     # Package utils
     "pacutils"
     # Network tools
-    "ethtool" "nmap" "nss-mdns" "openbsd-netcat" "openssh" "prettyping" "sshfs" "traceroute" "whois"
+    "ethtool" "nmap" "nss-mdns" "openbsd-netcat" "openssh" "prettyping" "sshfs" "traceroute" "wget" "whois"
     # Disk drive utils
     "hdparm" "smartmontools"
     # Monitors
