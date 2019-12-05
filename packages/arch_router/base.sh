f8c412b264870e62a5fb9203cfcee4df633f337d
Christian Muehlhaeuser <muesli@gmail.com>
2019-12-05 03:33:50 +0100

    Add arch_router flavor

---
+++ packages/arch_router/base.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PACKAGES=("${PACKAGES[@]}"
+    # Basic utils
+    "coreutils" "dnsutils" "dosfstools" "net-tools"
+    # Package utils
+    "pacutils"
+    # Network tools
+    "ethtool" "nmap" "nss-mdns" "openbsd-netcat" "openssh" "prettyping" "sshfs" "traceroute" "wget" "whois"
+    # Disk drive utils
+    "hdparm" "smartmontools"
+    # Monitors
+    "htop" "iotop" "lsof" "ncdu"
+    # Editors
+    "joe" "micro-bin"
+    # CLI tools
+    "ripgrep" "time" "tmux" "which"
+    # Decompression
+    "unrar" "unzip"
+    # Shell
+    "zsh"
+)
---
+++ packages/arch_router/dev.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PACKAGES=("${PACKAGES[@]}"
+    # SCMs
+    "git"
+    # Go
+    "go"
+    # Web-dev on CLI
+    "httpie"
+)
