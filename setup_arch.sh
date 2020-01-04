ca06469304c51f9678955529bbbde3e10f5b508d
Christian Muehlhaeuser <muesli@gmail.com>
2020-01-04 15:52:03 +0100

    Support multiple Arch flavors

--- setup_arch.sh
+++ setup_arch.sh
@@ -6,11 +6,21 @@
 #
 set -e
 
+if [[ "$FLAVOR" == "router" ]]; then
+    FLAVOR="_${FLAVOR}"
+elif [[ "$FLAVOR" == "minimal" ]]; then
+    FLAVOR="_${FLAVOR}"
+elif [[ "$FLAVOR" != "" ]]; then
+    # Unknown
+    echo "Sorry, but flavor '$FLAVOR' is unknown!"
+    exit 1
+fi
+
 # Install packages
-./packages/arch/packages.sh
+./packages/arch${FLAVOR}/packages.sh
 
 # Install drivers
-./packages/arch/drivers/bluetooth.sh
-./packages/arch/drivers/smartcard.sh
-#./packages/arch/drivers/intel.sh
-#./packages/arch/drivers/razer.sh
+#./packages/arch${FLAVOR}/drivers/bluetooth.sh
+#./packages/arch${FLAVOR}/drivers/smartcard.sh
+#./packages/arch${FLAVOR}/drivers/intel.sh
+#./packages/arch${FLAVOR}/drivers/razer.sh
