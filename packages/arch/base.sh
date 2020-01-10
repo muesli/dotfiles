a07de74b29f0ae100d34bd6ef26e564af0648c3a
Christian Muehlhaeuser <muesli@gmail.com>
2020-01-10 15:08:53 +0100

    Move network packages out of arch/base

--- packages/arch/base.sh
+++ packages/arch/base.sh
@@ -2,13 +2,11 @@
 
 PACKAGES=("${PACKAGES[@]}"
     # Basic utils
-    "coreutils" "dnsutils" "dosfstools" "net-tools"
+    "coreutils" "dnsutils" "dosfstools"
     # Package utils
     "pacutils"
     # System hooks
     "kernel-modules-hook"
-    # Network tools
-    "ethtool" "nmap" "nss-mdns" "openbsd-netcat" "openssh" "prettyping" "sshfs" "traceroute" "wget" "whois" "wol" "rsync"
     # Disk drive utils
     "hdparm" "smartmontools"
     # Monitors
---
+++ packages/arch/network.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PACKAGES=("${PACKAGES[@]}"
+    # Network tools
+    "net-tools" "inetutils" "ethtool" "nmap" "nss-mdns" "openbsd-netcat" "prettyping" "traceroute" "wget" "whois" "wol" "rsync"
+    # SSH
+    "openssh" "sshfs"
+)
