486618a80794761cd6f6517856b3a44a50b290ce
Christian Muehlhaeuser <muesli@gmail.com>
2021-01-10 18:40:57 +0100

    Add intel-gpu-tools package to intel drivers

--- packages/arch/drivers/intel.sh
+++ packages/arch/drivers/intel.sh
@@ -2,5 +2,5 @@
 
 # install intel packages
 yay -S --needed \
-    mesa intel-ucode vulkan-intel i7z libva-intel-driver
+    mesa intel-ucode vulkan-intel i7z libva-intel-driver intel-gpu-tools
 #    libva-utils xf86-video-intel
